7e3fd0f5e62465f925d671b871ba82af0aa21deb
Cris <cris@crisalb.com>
2023-01-10 10:39:25 -0800

    files

---
+++ src/main/java/com/uda/rest/dog/DogApplication.java
@@ -0,0 +1,12 @@
+package com.uda.rest.dog;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class DogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/uda/rest/dog/service/DogService.java
@@ -0,0 +1,12 @@
+package com.uda.rest.dog.service;
+
+import com.uda.rest.dog.entity.Dog;
+
+import java.util.List;
+
+public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
